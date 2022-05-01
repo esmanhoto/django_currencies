8ac16b72cc93ffc72f141b7acab2f1b2dc49e8f2
Eduardo Esmanhoto <eduardo_esmanhoto@hotmail.com>
2022-05-01 16:49:57 -0300

    model created and migrated

--- rates/models.py
+++ rates/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    date = models.DateField()
+    euro = models.CharField(max_length=20)
+    real = models.CharField(max_length=20)
+    yen = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.date)
