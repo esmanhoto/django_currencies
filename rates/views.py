4d6c95b07e7fa2bb21942bb0e31b6a5c42ab958c
Eduardo Esmanhoto <eduardo_esmanhoto@hotmail.com>
2022-05-01 16:46:18 -0300

    Base html and initial api_request added

--- rates/views.py
+++ rates/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    if request.method == "POST":
+        initial_date = request.POST['initial_date']
+        final_date = request.POST['final_date']
+
+        api_request = requests.get("https://api.vatcomply.com/rates?date=" + str(initial_date))
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:  # todo fix exception clause
+            api = "Error.."
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+
+        return render(request, 'home.html', {})
